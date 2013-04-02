de5f19d43d5b88bc5fa1ce9b62388bb9ddcb23ef
Pavel Lazureykis <lazureykis@gmail.com>
2013-04-02 14:24:15 +0400

    UPD branch info

--- lib/capistrano/jabber/notifications.rb
+++ lib/capistrano/jabber/notifications.rb
@@ -11,32 +11,40 @@ module Capistrano
       class << self
 
         attr_accessor :options
+        attr_accessor :variables
 
-        def deploy_started(stage)
-          send_jabber_message("cap deploy #{stage} by :username started at #{Time.now}")
+        def deploy_started
+          send_jabber_message "deploy"
         end
 
-        def deploy_completed(stage)
-          send_jabber_message("cap deploy #{stage} by :username completed at #{Time.now}")
+        def deploy_completed
+          send_jabber_message "deploy", true
         end
 
-        def rollback_started(stage)
-          send_jabber_message("cap deploy:rollback #{stage} by :username started at #{Time.now}")
+        def rollback_started
+          send_jabber_message "deploy:rollback"
         end
 
-        def rollback_completed(stage)
-          send_jabber_message("cap deploy:rollback #{stage} by :username completed at #{Time.now}")
+        def rollback_completed
+          send_jabber_message "deploy:rollback", true
         end
 
         private
 
-        def send_jabber_message(msg)
-          msg.gsub!(':username', username) if msg[':username']
+        def send_jabber_message(action, completed = false)
+          msg = []
+          msg << "#{completed ? 'Completed' : 'Started'} #{action} on #{variables[:stage]} by #{username}"
+          msg << "Time #{Time.now.strftime('%Y%jT%H%MZ')}"
+          msg << "Application #{variables[:application]}"
+          msg << "Branch #{variables[:branch]}"
+          msg << "Revision #{options[:real_revision]}"
+          msg << "Release name #{options[:release_name]}"
+          msg = msg.join("\r\n")
 
-          client = ::Jabber::Client.new(options[:uid])
-          client.connect(options[:server])
-          client.auth(options[:password])
-          notification_group = options[:group]
+          client = ::Jabber::Client.new(options[:uid].to_s)
+          client.connect(options[:server].to_s)
+          client.auth(options[:password].to_s)
+          notification_group = options[:group].to_s
 
           roster = ::Jabber::Roster::Helper.new(client)
 
@@ -46,7 +54,7 @@ module Capistrano
 
           roster.find_by_group(notification_group).each {|item|
             client.send(item.jid)
-            m = ::Jabber::Message.new(item.jid, msg).set_type(:normal).set_id('1').set_subject('hi')
+            m = ::Jabber::Message.new(item.jid, msg).set_type(:normal).set_id('1').set_subject('deploy')
             client.send(m)
           }
 
@@ -73,9 +81,13 @@ Capistrano::Configuration.instance(:must_exist).load do
             uid:      fetch(:jabber_uid),
             server:   fetch(:jabber_server),
             password: fetch(:jabber_password),
-            group:    fetch(:jabber_group)
+            group:    fetch(:jabber_group),
+            real_revision: fetch(:real_revision),
+            release_name: fetch(:release_name),
+            action: m.to_sym
           }
-          Capistrano::Jabber::Notifications.send(m, fetch(:stage))
+          Capistrano::Jabber::Notifications.variables = variables
+          Capistrano::Jabber::Notifications.send m
         end
       end
     end
